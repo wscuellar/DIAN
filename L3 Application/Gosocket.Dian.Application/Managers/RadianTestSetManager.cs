48033852923e6715534e2214c375ae36ae22f749
Roberto Alvarado <ralvaradot@hotmail.com>
2020-10-29 17:13:02 -0500

    Se crea Entidad RadianTestSet, el manager RadianTestSetManager, el controlador RadianTestSetController, Los view model RadiantestSetResultViewModel, RadianTestSetTableViewModel, RadianTestSetTrackingViewModel y radianTestSetViewModel. Las vistas del Controlador Index e _item. Se modifica Navigation

---
+++ L3 Application/Gosocket.Dian.Application/Managers/RadianTestSetManager.cs
@@ -0,0 +1,76 @@
+﻿using Gosocket.Dian.Domain.Common;
+using Gosocket.Dian.Domain.Entity;
+using Gosocket.Dian.Infrastructure;
+using Microsoft.WindowsAzure.Storage.Table;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gosocket.Dian.Application.Managers
+{
+    public class RadianTestSetManager
+    {
+        private static readonly TableManager testSetManager = new TableManager("RadianTestSet");
+
+        public bool InsertTestSet(RadianTestSet testSet)
+        {
+            return testSetManager.InsertOrUpdate(testSet);
+        }
+
+        //public bool InsertTestSetTracking(GlobalTestSetTracking testSetTracking)
+        //{
+        //    return testSetTrackingManager.Insert(testSetTracking);
+        //}
+
+        public IEnumerable<RadianTestSet> GetAllTestSet(/*string partitionKey*/)
+        {
+            try
+            {
+                TableContinuationToken token = null;
+                var testSets = new List<RadianTestSet>();
+                foreach (var operationModeId in new List<int> { (int)OperationMode.Free, (int)OperationMode.Own, (int)OperationMode.Provider })
+                {
+                    var data = testSetManager.GetRangeRows<RadianTestSet>($"{operationModeId}", 1000, token);
+                    testSets.AddRange(data.Item1);
+                }
+
+                return testSets;
+            }
+            catch (Exception ex)
+            {
+                //TODO: Crear o ubicar la clase que guarda logs de errores ¿Existe?
+                return new List<RadianTestSet>();
+            }
+        }
+
+        public GlobalTestSet GetTestSet(string partitionKey, string rowKey)
+        {
+            return testSetManager.Find<GlobalTestSet>(partitionKey, rowKey);
+        }
+
+        //public IEnumerable<GlobalTestSetTracking> GetAllTestSetTracking(string partitionKey)
+        //{
+        //    try
+        //    {
+        //        TableContinuationToken token = null;
+        //        var trackings = new List<GlobalTestSetTracking>();
+
+        //        do
+        //        {
+        //            var data = testSetTrackingManager.GetRangeRows<GlobalTestSetTracking>(partitionKey, 1000, token);
+        //            trackings.AddRange(data.Item1);
+        //        }
+        //        while (token != null);
+
+        //        return trackings;
+        //    }
+        //    catch (Exception ex)
+        //    {
+        //        return new List<GlobalTestSetTracking>();
+        //    }
+        //}
+
+    }
+}
---
+++ L4 Presentation/Web/Gosocket.Dian.Web/Models/RadianTestSetViewModel.cs
@@ -0,0 +1,158 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gosocket.Dian.Web.Models
+{
+    public class RadianTestSetViewModel
+    {
+
+        public string TestSetId { get; set; }
+        public int Status { get; set; }
+        public bool TestSetReplace { get; set; }
+
+        [Required(ErrorMessage = "La descripción es requerida")]
+        [Display(Name = "Descripción")]
+        public string Description { get; set; }
+
+        public int OperationModeId { get; set; }
+        public string OperationModeName { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Documentos")]
+        public int TotalDocumentRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Documentos aceptados")]
+        public int TotalDocumentAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Acuse de recibo")]
+        public int ReceiptNoticeTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Acuse de recibo aceptados")]
+        public int ReceiptNoticeTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Recibo del bien o Prestación del servicio")]
+        public int ReceiptServiceTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Recibo del bien o Prestación del servicio")]
+        public int ReceiptServiceTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Aceptación expresa")]
+        public int ExpressAcceptanceTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Aceptación expresa")]
+        public int ExpressAcceptanceTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Manifestación de aceptación automática")]
+        public int AutomaticAcceptanceTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Manifestación de aceptación automática")]
+        public int AutomaticAcceptanceTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Rechazo factura electrónica de venta")]
+        public int RejectInvoiceTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Manifestación de aceptación automática")]
+        public int RejectInvoiceTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Solicitud de disponibilización")]
+        public int ApplicationAvailableTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Solicitud de disponibilización")]
+        public int ApplicationAvailableTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Endoso electrónico")]
+        public int EndorsementTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Endoso electrónico")]
+        public int EndorsementTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Cancelación de endoso electrónico")]
+        public int EndorsementCancellationTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Cancelación de endoso electrónico")]
+        public int EndorsementCancellationTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Avales")]
+        public int GuaranteeTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Avales")]
+        public int GuaranteeTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Mandato electrónico")]
+        public int ElectronicMandateTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Manadato electrónico")]
+        public int ElectronicMandateTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Terminación mandato electrónico")]
+        public int EndMandateTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Terminación manadato electrónico")]
+        public int EndMandateTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Notificación de pago parcial o total")]
+        public int PaymentNotificationTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Notificación pago parcial o total")]
+        public int PaymnetNotificationTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Limitación de circulación")]
+        public int CirculationLimitationTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Limitación de circulación")]
+        public int CirculationLimitationTotalAcceptedRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Terminación limitación de circulación")]
+        public int EndCirculationLimitationTotalRequired { get; set; }
+
+        [Required(ErrorMessage = "El campo es requerido")]
+        [Display(Name = "Terminación limitación de circulación")]
+        public int EndCirculationLimitationTotalAcceptedRequired { get; set; }
+
+        public DateTime Date { get; set; }
+
+        public string CreatedBy { get; set; }
+        public string UpdateBy { get; set; }
+        public bool Active { get; set; }
+
+        public List<OperationModeViewModel> GetOperationModes()
+        {
+            return new List<OperationModeViewModel>
+            {
+                new OperationModeViewModel{ Id = 1, Name = "Software Propio" },
+                new OperationModeViewModel{ Id = 2, Name = "Software de un Proveedor Tecnológico" },
+                new OperationModeViewModel{ Id = 3, Name = "Software de un Sistema de Negociación" },
+                new OperationModeViewModel{ Id = 4, Name = "Software de un Factor" }
+            };
+        }
+    }
+}
