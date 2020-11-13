744195554d1c7ffbe3c6d246530a8fd96a7d5fb5
Fernando Ibañez Gutierrez <fernandoibanez@creasistemas.com>
2020-11-13 17:07:37 -0500

    HU-020

--- L4 Presentation/Web/Gosocket.Dian.Web/Controllers/RadianApprovedController.cs
+++ L4 Presentation/Web/Gosocket.Dian.Web/Controllers/RadianApprovedController.cs
@@ -1,7 +1,9 @@
 ﻿using Gosocket.Dian.Domain.Common;
+using Gosocket.Dian.Domain.Entity;
 using Gosocket.Dian.Interfaces.Services;
 using Gosocket.Dian.Web.Common;
 using Gosocket.Dian.Web.Models;
+using Gosocket.Dian.Web.Models.RadianApproved;
 using System.Collections.Generic;
 using System.Web.Mvc;
 
@@ -9,11 +11,10 @@ namespace Gosocket.Dian.Web.Controllers
 {
     public class RadianApprovedController : Controller
     {
-
         private readonly IRadianContributorService _radianContributorService;
         private readonly IRadianTestSetService _radianTestSetService;
 
-        public RadianApprovedController(IRadianContributorService radianContributorService, IRadianTestSetService radianTestSetService, IRadianContributorFileTypeService radianContributorFileTypeService)
+        public RadianApprovedController(IRadianContributorService radianContributorService, IRadianTestSetService radianTestSetService)
         {
             _radianContributorService = radianContributorService;
             _radianTestSetService = radianTestSetService;
@@ -22,7 +23,7 @@ namespace Gosocket.Dian.Web.Controllers
         [HttpGet]
         public ActionResult Index()
         {
-           // LoadSoftwareModeOperation();
+            // LoadSoftwareModeOperation();
             return View();
         }
 
@@ -44,9 +45,15 @@ namespace Gosocket.Dian.Web.Controllers
             // CA 2.4 
             // Software de un Proveedor Electronico
 
-
-
-            return View();
+            RadianAdmin radianAdmin = _radianContributorService.ContributorSummary(registrationData.ContributorId);
+            return View(new RadianApprovedViewModel()
+            {
+                Name = radianAdmin.Contributor.TradeName,
+                Nit = radianAdmin.Contributor.Code,
+                BusinessName = radianAdmin.Contributor.BusinessName,
+                Email = radianAdmin.Contributor.Email,
+                Files = radianAdmin.Files
+            });
         }
 
         private void LoadSoftwareModeOperation()
