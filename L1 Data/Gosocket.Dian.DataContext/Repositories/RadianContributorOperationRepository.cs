6fa1d9f9cd6b9f35739379a6db10f42c27018402
Manquip <manquipa@hotmail.com>
2020-11-14 09:01:09 -0500

    Repositorio e interface de RadianContributorOperation

---
+++ L1 Data/Gosocket.Dian.DataContext/Repositories/RadianContributorOperationRepository.cs
@@ -0,0 +1,31 @@
+﻿using Gosocket.Dian.Domain;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Linq.Expressions;
+
+namespace Gosocket.Dian.DataContext.Repositories
+{
+    public class RadianContributorOperationRepository : IRadianContributorOperationRepository
+    {
+        private readonly SqlDBContext sqlDBContext;
+
+        public RadianContributorOperationRepository()
+        {
+            if (sqlDBContext == null)
+                sqlDBContext = new SqlDBContext();
+        }
+
+        public List<RadianContributorOperation> List(Expression<Func<RadianContributorOperation, bool>> expression)
+        {
+            var query = sqlDBContext.RadianContributorOperations.Where(expression);
+            return query.ToList();
+        }
+
+        public RadianContributorOperation Get(Expression<Func<RadianContributorOperation, bool>> expression)
+        {
+            var query = sqlDBContext.RadianContributorOperations.Where(expression);
+            return query.FirstOrDefault();
+        }
+    }
+}
