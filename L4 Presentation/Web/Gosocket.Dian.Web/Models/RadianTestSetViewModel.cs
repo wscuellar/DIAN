7805787ba6dc62113be845c32d4f6d0c8444108f
Roberto Alvarado <ralvaradot@hotmail.com>
2020-11-17 12:01:05 -0500

    Se corrige vista Add con los bugs reportados

--- L4 Presentation/Web/Gosocket.Dian.Web/Models/RadianTestSetViewModel.cs
+++ L4 Presentation/Web/Gosocket.Dian.Web/Models/RadianTestSetViewModel.cs
@@ -1,5 +1,4 @@
-﻿using Gosocket.Dian.Interfaces.Repositories;
-using System;
+﻿using System;
 using System.Collections.Generic;
 using System.ComponentModel.DataAnnotations;
 
@@ -29,114 +28,170 @@ namespace Gosocket.Dian.Web.Models
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Acuse de recibo")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ReceiptNoticeTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Acuse de recibo aceptados")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ReceiptNoticeTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Recibo del bien ")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ReceiptServiceTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Recibo del bien ")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ReceiptServiceTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Aceptación expresa")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ExpressAcceptanceTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Aceptación expresa")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ExpressAcceptanceTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Manifestación de aceptación")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int AutomaticAcceptanceTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Manifestación de aceptación")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int AutomaticAcceptanceTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Rechazo factura electrónica")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int RejectInvoiceTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Rechazo factura electrónica")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int RejectInvoiceTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Solicitud de disponibilización")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ApplicationAvailableTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Solicitud de disponibilización")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ApplicationAvailableTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Endoso electrónico")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndorsementTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Endoso electrónico")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndorsementTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Cancelación de endoso")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndorsementCancellationTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Cancelación de endoso")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndorsementCancellationTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Avales")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int GuaranteeTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Avales")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int GuaranteeTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Mandato electrónico")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ElectronicMandateTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Manadato electrónico")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int ElectronicMandateTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Terminación mandato")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndMandateTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Terminación manadato")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndMandateTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Notificación de pago")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int PaymentNotificationTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Notificación pago")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int PaymentNotificationTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Limitación de circulación")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int CirculationLimitationTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Limitación de circulación")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int CirculationLimitationTotalAcceptedRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Terminación limitación")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndCirculationLimitationTotalRequired { get; set; }
 
         [Required(ErrorMessage = "El campo es requerido")]
         [Display(Name = "Terminación limitación")]
+        [Range(0, short.MaxValue, ErrorMessage = "El valor {0} debe ser max 32767.")]
+        [RegularExpression("([0-9]+)", ErrorMessage = "El valor {0} debe ser numérico")]
         public int EndCirculationLimitationTotalAcceptedRequired { get; set; }
 
         public DateTime Date { get; set; }
