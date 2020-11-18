326dc6f012ac8f2bd32cfa93a9388b39cf423f9c
Fernando Ibañez Gutierrez <fernandoibanez@creasistemas.com>
2020-11-18 18:46:03 -0500

    Changes radian factor operation mode

---
+++ L4 Presentation/Web/Gosocket.Dian.Web/Models/RadianApproved/RadianApprovedOperationModeViewModel.cs
@@ -0,0 +1,20 @@
+﻿using Gosocket.Dian.Domain;
+using System.ComponentModel.DataAnnotations;
+
+namespace Gosocket.Dian.Web.Models.RadianApproved
+{
+    public class RadianApprovedOperationModeViewModel
+    {
+        [Display(Name = "Configuración modo de Operación")]
+        public RadianOperationMode OperationModeSelected { get; set; }
+
+        [Display(Name = "URL de recepción de eventos")]
+        public string Url { get; set; }
+
+        [Display(Name = "Nombre de software")]
+        public string SoftwareName { get; set; }
+
+        [Display(Name = "PIN del SW")]
+        public string SoftwarePin { get; set; }
+    }
+}
