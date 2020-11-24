2d936076a9523204e0e14579135fe6e0693a0d6f
Juan Carlos <jcramosd@indracompany.com>
2020-11-24 16:09:03 -0500

    Ajustes transversal accesso

--- L1 Data/Gosocket.Dian.DataContext/Repositories/RadianContributorRepository.cs
+++ L1 Data/Gosocket.Dian.DataContext/Repositories/RadianContributorRepository.cs
@@ -57,6 +57,7 @@ namespace Gosocket.Dian.DataContext.Repositories
                     radianContributorInstance.RadianState = radianContributor.RadianState;
                     radianContributorInstance.RadianOperationModeId = radianContributor.RadianOperationModeId;
                     radianContributorInstance.CreatedBy = radianContributor.CreatedBy;
+                    radianContributorInstance.Description = radianContributor.Description;
                     radianContributorInstance.Step = radianContributor.Step == 0 ? 1 : radianContributor.Step;
 
                     context.Entry(radianContributorInstance).State = System.Data.Entity.EntityState.Modified;
