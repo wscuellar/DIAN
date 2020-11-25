6541ffbf318abe645c3685ef79857745713b1a9d
Camilo Lizarazo <clizarazo@likeu.co>
2020-11-25 14:40:42 -0500

    HU020-3 dropdown Autocomplete

---
+++ L4 Presentation/Web/Gosocket.Dian.Web/Content/js/Radian/RadianOperationMode.js
@@ -0,0 +1,52 @@
+﻿function DeleteOperationMode(url) {
+    $("#delete-software").click(function () {
+        var metod = 'POST';
+        var data = {
+            Id: $(this).attr("data-id")
+        }
+        var actionError = () => { }
+        var actionSuccess = () => {
+            location.reload();
+        }
+        ajaxFunction(url, metod, data, actionError, actionSuccess)
+    })
+}
+
+function AddOperationMode(url, contributorId, radianTypeId) {
+    $("#save-operation-mode").click(function () {
+        var metod = 'POST';
+        var data = {
+            ContributorId: contributorId,
+            RadianTypeId: radianTypeId
+        }
+        var actionError = () => { }
+        var actionSuccess = () => {
+            location.reload();
+        }
+        ajaxFunction(url, metod, data, actionError, actionSuccess)
+    })
+}
+
+function RenderAutocomplete() {
+    const myData = [{
+        "id": 1,
+        "name": 'Item 1',
+        "ignore": false
+    }, {
+        "id": 2,
+        "name": 'Item 2',
+        "ignore": false
+    }, {
+        "id": 3,
+        "name": 'Item 3',
+        "ignore": false
+    },
+        // ...
+    ]
+
+    $('.demo').autocomplete({
+        nameProperty: 'name',
+        valueField: '#hidden-field',
+        dataSource: myData
+    });
+}
