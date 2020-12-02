ae513d561b92e99db99100c9688bfeb0d6b60cd1
Mauricio Moreno <mauriciomoreno.mq@gmail.com>
2020-12-01 20:16:51 -0500

    Actualización servicio de generación de reporte de factura electrónica Radian

--- L3 Application/Gosocket.Dian.Application/RadianPDFCreationService.cs
+++ L3 Application/Gosocket.Dian.Application/RadianPDFCreationService.cs
@@ -1,12 +1,17 @@
-﻿using Gosocket.Dian.Interfaces.Services;
+﻿using Gosocket.Dian.Application.Cosmos;
+using Gosocket.Dian.Common.Resources;
+using Gosocket.Dian.Domain.Cosmos;
+using Gosocket.Dian.Infrastructure;
+using Gosocket.Dian.Interfaces.Services;
 using OpenHtmlToPdf;
 using QRCoder;
 using System;
 using System.Collections.Generic;
-using System.Configuration;
 using System.Drawing;
-using System.IO;
+using System.Globalization;
+using System.Linq;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Gosocket.Dian.Application
 {
@@ -15,70 +20,58 @@ namespace Gosocket.Dian.Application
         #region Properties
 
         private readonly IQueryAssociatedEventsService _queryAssociatedEventsService;
+        private readonly FileManager _fileManager;
 
-
-        #region RadianReportQRCodeRadianReportQRCodeUrl
-
-        private string RadianReportQRCodeRadianReportQRCodeUrl
-        {
-            get
-            {
-                if (ConfigurationManager.AppSettings["RadianReportQRCodeUrl"] != null)
-                {
-                    return ConfigurationManager.AppSettings["RadianReportQRCodeUrl"].ToString();
-                }
-                return string.Empty;
-            }
-        } 
-
-        #endregion
 
         #endregion
 
         #region Constructor
 
-        public RadianPDFCreationService(IQueryAssociatedEventsService queryAssociatedEventsService)
+        public RadianPDFCreationService(IQueryAssociatedEventsService queryAssociatedEventsService, FileManager fileManager)
         {
             _queryAssociatedEventsService = queryAssociatedEventsService;
-        } 
+            _fileManager = fileManager;
+        }
 
         #endregion
 
         #region GetElectronicInvoicePdf
 
-        public byte[] GetElectronicInvoicePdf(string eventItemIdentifier)
+        public async Task<byte[]> GetElectronicInvoicePdf(string eventItemIdentifier)
         {
-            // Load Templates
-            StringBuilder templateFirstPage = new StringBuilder(File.ReadAllText("../../Templates/CertificadoExistencia.html"));
-            StringBuilder eventTemplate = new StringBuilder(File.ReadAllText("../../Templates/CertificadoExistenciaInterna.html"));
-            StringBuilder templateLastPage = new StringBuilder(File.ReadAllText("../../Templates/CertificadoExistenciaFinal.html"));
+            // Load Templates           
+
+            StringBuilder templateFirstPage = new StringBuilder(_fileManager.GetText("radian-documents-templates", "CertificadoExistencia.html"));
+            StringBuilder templateLastPage = new StringBuilder(_fileManager.GetText("radian-documents-templates", "CertificadoExistenciaFinal.html"));
 
 
             // Load Document Data
             GlobalDocValidatorDocumentMeta documentMeta = _queryAssociatedEventsService.DocumentValidation(eventItemIdentifier);
 
             //Load documents
-            List<GlobalDocReferenceAttorney> documents = _queryAssociatedEventsService.ReferenceAttorneys(documentMeta.DocumentKey, documentMeta.DocumentReferencedKey, documentMeta.ReceiverCode, documentMeta.SenderCode);
+            List<GlobalDocReferenceAttorney> documents =
+                _queryAssociatedEventsService.ReferenceAttorneys(
+                    documentMeta.DocumentKey,
+                    documentMeta.DocumentReferencedKey,
+                    documentMeta.ReceiverCode,
+                    documentMeta.SenderCode);
 
             //Load Events
-            
+            GlobalDataDocument cosmosDocument = await DocumentInfoFromCosmos(documentMeta);
+            List<Event> events = cosmosDocument.Events;
 
             // Set Variables
             DateTime expeditionDate = DateTime.Now;
-            Bitmap qrCode = GenerateQR(string.Format("{0}{1}", this.RadianReportQRCodeRadianReportQRCodeUrl, documentMeta.PartitionKey));
+            Bitmap qrCode = GenerateQR(TextResources.RadianReportQRCode.Replace("{CUFE}", documentMeta.PartitionKey));
             int page = 1;
-            int eventNumber = 1;
+
+            string ImgDataURI = IronPdf.Util.ImageToDataUri(qrCode);
+            string ImgHtml = String.Format("<img class='qr-content' src='{0}'>", ImgDataURI);
+
 
             // Mapping Labels common data
-            templateFirstPage = templateFirstPage.Replace("{PrintDay}", expeditionDate.Day.ToString());
-            templateFirstPage = templateFirstPage.Replace("{PrintMonth}", expeditionDate.Month.ToString());
-            templateFirstPage = templateFirstPage.Replace("{PrintYear}", expeditionDate.Year.ToString());
-            templateFirstPage = templateFirstPage.Replace("{PrintTime}", expeditionDate.TimeOfDay.ToString());
-            templateFirstPage = templateFirstPage.Replace("{PrintPage}", page.ToString());
-            templateFirstPage = templateFirstPage.Replace("{InvoiceNumber}", documentMeta.SerieAndNumber);
-            templateFirstPage = templateFirstPage.Replace("{CUFE}", documentMeta.PartitionKey);
-            templateFirstPage = templateFirstPage.Replace("{EInvoiceGenerationDate}", documentMeta.EmissionDate.ToString("yyyy-mm-dd HH:mm:ss.sss"));
-            templateFirstPage = templateFirstPage.Replace("{Status}", string.Empty);
+
+            templateFirstPage = CommonDataTemplateMapping(templateFirstPage, expeditionDate, page, documentMeta);
 
             // Mapping firts page
             templateFirstPage = templateFirstPage.Replace("{SenderBusinessName}", documentMeta.SenderName);
@@ -91,31 +84,59 @@ namespace Gosocket.Dian.Application
             templateFirstPage = templateFirstPage.Replace("{ReceiverBusinessName}", documentMeta.ReceiverName);
             templateFirstPage = templateFirstPage.Replace("{ReceiverNit}", documentMeta.ReceiverCode);
 
-            //Mapping last page
-            templateLastPage = templateLastPage.Replace("{PrintDay}", expeditionDate.Day.ToString());
-            templateLastPage = templateLastPage.Replace("{PrintMonth}", expeditionDate.Month.ToString());
-            templateLastPage = templateLastPage.Replace("{PrintYear}", expeditionDate.Year.ToString());
-            templateLastPage = templateLastPage.Replace("{PrintTime}", expeditionDate.TimeOfDay.ToString());
-            templateLastPage = templateLastPage.Replace("{PrintPage}", page.ToString());
-            templateLastPage = templateLastPage.Replace("{InvoiceNumber}", documentMeta.SerieAndNumber);
-            templateLastPage = templateLastPage.Replace("{CUFE}", documentMeta.PartitionKey);
-            templateLastPage = templateLastPage.Replace("{EInvoiceGenerationDate}", documentMeta.EmissionDate.ToString("yyyy-mm-dd HH:mm:ss.sss"));
-            templateLastPage = templateLastPage.Replace("{Status}", string.Empty);
 
-            templateLastPage = templateLastPage.Replace("{DocumentsTotal}", $"{documents.Count}");
-            templateLastPage = templateLastPage.Replace("{EventsTotal}", string.Empty);
-            templateLastPage = templateLastPage.Replace("{ExpeditionDate}", expeditionDate.ToShortDateString());
-            templateLastPage = templateLastPage.Replace("{QRCode}", qrCode.ToString());
 
             // Mapping Events
-                        
 
-            // Render Pdf
-            byte[] report = GetPdfBytes(templateFirstPage.ToString());
-            //
+            // se realiza el mapeo del primer evento
+            if (events.Any())
+            {
+                templateFirstPage = EventTemplateMapping(templateFirstPage, events[0], string.Empty);
+
+                // si tiene más eventos realiza el mapeo del siguiente template
+                if (events.Count > 1)
+                {
+                    bool changePage = false;
+                    StringBuilder middleTemplate = new StringBuilder();
+
+                    for (int i = 1; i < events.Count; i++)
+                    {
+                        if (i % 2 == 1)
+                        {
+                            page++;
+                            middleTemplate = new StringBuilder(_fileManager.GetText("radian-documents-templates", "CertificadoExistenciaInterna.html"));
+                            middleTemplate = CommonDataTemplateMapping(middleTemplate, expeditionDate, page, documentMeta);
+
+                        }
+                        else
+                        {
+                            changePage = true;
+                        }
+                        middleTemplate = EventTemplateMapping(middleTemplate, events[i], changePage ? string.Empty : "1");
+                        if (changePage)
+                        {
+                            templateFirstPage = templateFirstPage.Append(middleTemplate);
+                            changePage = false;
+                        }
+                    }
+                }
+            }
+
+            //Mapping last page
+
+            // se aumenta el número de la pagina y se mapean los datos comunes de pagina
+            page++;
+            templateLastPage = CommonDataTemplateMapping(templateLastPage, expeditionDate, page, documentMeta);
+
+            templateLastPage = templateLastPage.Replace("{DocumentsTotal}", documents.Count.ToString());
+            templateLastPage = templateLastPage.Replace("{EventsTotal}", events.Count.ToString());
+            templateLastPage = templateLastPage.Replace("{ExpeditionDate}", expeditionDate.ToShortDateString());
+            templateLastPage = templateLastPage.Replace("{QRCode}", ImgHtml);
+
+            byte[] report = GetPdfBytes(templateFirstPage.Append(templateLastPage.ToString()).ToString());
 
             return report;
-        } 
+        }
 
         #endregion
 
@@ -140,15 +161,75 @@ namespace Gosocket.Dian.Application
 
         #region GenerateQR
 
-        public Bitmap GenerateQR(string invoiceUrl)
+        private Bitmap GenerateQR(string invoiceUrl)
         {
             QRCodeGenerator qrGenerator = new QRCodeGenerator();
             QRCodeData qrCodeData = qrGenerator.CreateQrCode(invoiceUrl, QRCodeGenerator.ECCLevel.Q);
             QRCode qrCode = new QRCode(qrCodeData);
             Bitmap qrCodeImage = qrCode.GetGraphic(20);
             return qrCodeImage;
-        } 
+        }
 
         #endregion
+
+        #region DocumentInfoFromCosmos
+
+        private async Task<GlobalDataDocument> DocumentInfoFromCosmos(GlobalDocValidatorDocumentMeta documentMeta)
+        {
+            string emissionDateNumber = documentMeta.EmissionDate.ToString("yyyyMMdd");
+            string partitionKey = $"co|{emissionDateNumber.Substring(6, 2)}|{documentMeta.DocumentKey.Substring(0, 2)}";
+
+            DateTime date = DateNumberToDateTime(emissionDateNumber);
+
+            GlobalDataDocument globalDataDocument = await CosmosDBService.Instance(date).ReadDocumentAsync(documentMeta.DocumentKey, partitionKey, date);
+
+            return globalDataDocument;
+        }
+
+        #endregion
+
+        #region DateNumberToDateTime
+
+        private DateTime DateNumberToDateTime(string date)
+        {
+            return DateTime.ParseExact(date, "yyyyMMdd", CultureInfo.InvariantCulture);
+        }
+
+        #endregion
+
+        #region CommonDataTemplateMapping
+
+        private StringBuilder CommonDataTemplateMapping(StringBuilder template, DateTime expeditionDate, int page, GlobalDocValidatorDocumentMeta documentMeta)
+        {
+            template = template.Replace("{PrintDay}", expeditionDate.Day.ToString());
+            template = template.Replace("{PrintMonth}", expeditionDate.Month.ToString());
+            template = template.Replace("{PrintYear}", expeditionDate.Year.ToString());
+            template = template.Replace("{PrintTime}", expeditionDate.TimeOfDay.ToString());
+            template = template.Replace("{PrintPage}", page.ToString());
+            template = template.Replace("{InvoiceNumber}", documentMeta.SerieAndNumber);
+            template = template.Replace("{CUFE}", documentMeta.PartitionKey);
+            template = template.Replace("{EInvoiceGenerationDate}", documentMeta.EmissionDate.ToString("yyyy-mm-dd HH:mm:ss.sss"));
+            template = template.Replace("{Status}", string.Empty);
+            return template;
+        }
+
+        #endregion
+
+        #region EventTemplateMapping
+
+        private StringBuilder EventTemplateMapping(StringBuilder template, Event eventObj, string subEvent)
+        {
+            template = template.Replace("{EventNumber" + subEvent + "}", eventObj.DateNumber.ToString());
+            template = template.Replace("{DocumentTypeName" + subEvent + "}", eventObj.Description);
+            template = template.Replace("{CUDE" + subEvent + "}", eventObj.Code);
+            template = template.Replace("{ValidationDate" + subEvent + "}", eventObj.Date.ToString("yyyy-mm-dd HH:mm:ss.sss"));
+            template = template.Replace("{SenderBusinessName" + subEvent + "}", eventObj.SenderName);
+            template = template.Replace("{ReceiverBusinessName" + subEvent + "}", eventObj.ReceiverName);
+
+            return template;
+        }
+
+        #endregion
+
     }
 }
