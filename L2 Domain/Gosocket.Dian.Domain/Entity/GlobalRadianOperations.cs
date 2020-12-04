14510a5d9e228d84d9e766992d663a46bc2c18aa
Juan Carlos <jcramosd@indracompany.com>
2020-12-04 11:48:31 -0500

    adicion de servicio para el manejo de GlobalRadianOperations

---
+++ L2 Domain/Gosocket.Dian.Domain/Entity/GlobalRadianOperations.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.WindowsAzure.Storage.Table;
+
+namespace Gosocket.Dian.Domain.Entity
+{
+    public class GlobalRadianOperations : TableEntity
+    {
+
+        public GlobalRadianOperations() { }
+
+        public GlobalRadianOperations(string code, string softwareId) : base(code, softwareId)
+        {
+            PartitionKey = code; // track id zip
+            RowKey = softwareId; // track id xml
+        }
+
+        public string SoftwareType { get; set; }
+
+        public string RadianStatus { get; set; }
+        
+    }
+}
