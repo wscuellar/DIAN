21d1212622491f6a5ded18f87d5b7682f0c69610
=rmaestre <=ingmaestrericardo@gmail.com>
2020-12-12 09:28:31 -0500

    RM: Controlador de otros doc electronicos para cuando este asociado

---
+++ L4 Presentation/Web/Gosocket.Dian.Web/Controllers/OthersElectronicDocAssociatedController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace Gosocket.Dian.Web.Controllers
+{
+    public class OthersElectronicDocAssociatedController : Controller
+    {
+        // GET: OthersElectronicDocAssociated
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
