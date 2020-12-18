381b237fb080fbef47bdd39d09b95aa7f6a1d30c
Wilson Dario Campaña Diaz <wdario.c7@gmail>
2020-12-18 14:12:40 -0500

    Dario Campaña: Update DropDownList Tipo_documento

--- L4 Presentation/Web/Gosocket.Dian.Web/Controllers/FreeBillerController.cs
+++ L4 Presentation/Web/Gosocket.Dian.Web/Controllers/FreeBillerController.cs
@@ -97,6 +97,9 @@ namespace Gosocket.Dian.Web.Controllers
             var algo = model;
             return RedirectToAction("FreeBillerUser");
         }
+
+
+
         /// <summary>
         /// Cargue de información para editar.
         /// </summary>
@@ -105,6 +108,9 @@ namespace Gosocket.Dian.Web.Controllers
         [HttpGet]
         public ActionResult EditFreeBillerUser(string guid ="7713a5d0-fe76-4e9a-b704-d05a7d7b7f07")
         {
+            var tdocs = GetTypesDoc();
+            //dropdown
+            ViewBag.tdocs = tdocs.Select(p => new SelectListItem() { Value = p.Value.ToString(), Text = p.Text }).ToList<SelectListItem>();
             UserService user = new UserService();
             var data = user.Get(guid);
             UserFreeBillerModel model = new UserFreeBillerModel();
@@ -112,15 +118,13 @@ namespace Gosocket.Dian.Web.Controllers
             model.Name = data.UserName;
             model.Email = data.Email;
             model.LastUpdate = data.LastUpdated;
-            model.Profiles = this.DataPerfiles();
+            model.Profiles = this.GetProfiles();
             model.LastName = "Perez";
             model.FullName = data.Name;
             model.NumberDoc = data.IdentificationId;
             model.ProfileId = 1;
             model.TypeDocId = Convert.ToString(data.IdentificationTypeId);
             model.IsActive = false;
-
-
             return View(model);
         }
         /// <summary>
@@ -146,7 +150,7 @@ namespace Gosocket.Dian.Web.Controllers
                 if (result.Succeeded)
                 {
                     //Envio de notificacion por correo
-                    var envio = SendMailCreate(model);
+                    SendMailCreate(model);
                     return RedirectToAction("FreeBillerUser");
                 }
                 foreach (var error in result.Errors)
@@ -160,8 +164,12 @@ namespace Gosocket.Dian.Web.Controllers
         public ActionResult CreateUser()
         {
             UserFreeBillerModel model = new UserFreeBillerModel();
+
             model.TypesDoc = this.staticTypeDoc;
             model.Profiles = this.staticProfiles;
+
+            model.TypesDoc = this.GetTypesDoc();
+            model.Profiles = this.GetProfiles();
 
             return View(model);
         }
@@ -170,8 +178,6 @@ namespace Gosocket.Dian.Web.Controllers
         public ActionResult CreateUser(UserFreeBillerModel model)
         {
             var uCompany = userService.Get(User.Identity.GetUserId());
-
-
             if (!ModelState.IsValid)
             {
                 IEnumerable<ModelError> allErrors = ModelState.Values.SelectMany(v => v.Errors);
@@ -361,6 +367,7 @@ namespace Gosocket.Dian.Web.Controllers
             return selectTypesId;
         }
 
+    
         /// <summary>
         /// Método encargado de obtener los tipos de documentos y asignarlos a una lista para 
         /// usarlos en las vistas.
@@ -419,4 +426,5 @@ namespace Gosocket.Dian.Web.Controllers
             return listUsers;
         }
     }
+    
 }
