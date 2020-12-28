ff9a5fd78d484f1001ee2e0d1adc8fe1a2021aa9
Mauricio Moreno <mauriciomoreno.mq@gmail.com>
2020-12-28 16:33:34 -0500

    Creacion artefactos para documento soporte

---
+++ L3 Application/Gosocket.Dian.Application/RadianSupportDocument.cs
@@ -0,0 +1,22 @@
+﻿using Gosocket.Dian.Application.Cosmos;
+using Gosocket.Dian.Infrastructure;
+using Gosocket.Dian.Interfaces.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gosocket.Dian.Application
+{
+    public class RadianSupportDocument : IRadianSupportDocument
+    {
+        #region Properties
+
+        private readonly IQueryAssociatedEventsService _queryAssociatedEventsService;
+        private readonly FileManager _fileManager;
+        private readonly CosmosDBService _cosmosDBService;
+
+        #endregion
+    }
+}
