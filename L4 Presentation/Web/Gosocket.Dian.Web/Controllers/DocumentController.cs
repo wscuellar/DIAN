d1a18361e15d1c015e995511947f29c603df75c9
Manquip <manquipa@hotmail.com>
2020-12-28 12:54:11 -0500

    Ajustes a iconos y a link de descarga de PDF de factura

--- L4 Presentation/Web/Gosocket.Dian.Web/Controllers/DocumentController.cs
+++ L4 Presentation/Web/Gosocket.Dian.Web/Controllers/DocumentController.cs
@@ -82,7 +82,8 @@ namespace Gosocket.Dian.Web.Controllers
 
         [CustomRoleAuthorization(CustomRoles = "Proveedor")]
         public async Task<ActionResult> Provider(SearchDocumentViewModel model) => await GetDocuments(model, 4);
-               
+
+        [ExcludeFilter(typeof(Authorization))]
         public async Task<ActionResult> Details(string trackId)
         {
             DocValidatorModel model = await ReturnDocValidatorModelByCufe(trackId);
