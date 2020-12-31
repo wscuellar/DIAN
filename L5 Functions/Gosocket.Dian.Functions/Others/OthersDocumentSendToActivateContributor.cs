74252ed1cd71b5671dfa0b091527c1d27ed22cf6
Anthony Rivera <anriverav@eservicios.indracompany.com>
2020-12-31 09:22:59 -0500

    REQ: Contributor Class

---
+++ L5 Functions/Gosocket.Dian.Functions/Others/OthersDocumentSendToActivateContributor.cs
@@ -0,0 +1,12 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gosocket.Dian.Functions.Others
+{
+    class OthersDocumentSendToActivateContributor
+    {
+    }
+}
