d4469f9deb011c8287865bcaf98c31cddc09c352
Camilo Lizarazo <clizarazo@likeu.co>
2021-01-18 12:00:14 -0500

    fix freebiller

--- L4 Presentation/Web/Gosocket.Dian.Web/Content/js/FreeBiller/FreeBiller.js
+++ L4 Presentation/Web/Gosocket.Dian.Web/Content/js/FreeBiller/FreeBiller.js
@@ -2,3 +2,22 @@
 function createUser(url, metod, data, actionError, actionSuccess) {
     ajaxFunction(url, metod, data, actionError, actionSuccess);
 }
+
+function initialValuesCheck(ProfileId, url) {
+
+    var dataAjax = {
+        profileId: ProfileId
+    }
+    var errorAction = (error) => {
+        console.log(error);
+    }
+    var successAction = (result) => {
+        result.forEach(function (id) {
+            $("input#" + id).prop("checked", "checked");
+        });
+    }
+    ajaxFunction(url, "POST", dataAjax, errorAction, successAction);
+
+    //var newModel = JSON.parse(model.replace(/(&quot\;)/g, "\""));
+    //console.log(newModel);
+}
