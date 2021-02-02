50d99d684e70771e526fecc83d9213c74414db7b
Camilo Lizarazo <clizarazo@likeu.co>
2021-02-02 15:46:52 -0500

    fix remember password

--- L4 Presentation/Web/Gosocket.Dian.Web/Content/js/user/forgot-password.js
+++ L4 Presentation/Web/Gosocket.Dian.Web/Content/js/user/forgot-password.js
@@ -6,26 +6,32 @@
             size: 'medium'
         })
         box.bind('shown.bs.modal', function () {
-            $("#submitRememberPassword").click(function (e) {debugger
+            $("#submitRememberPassword").click(function (e) {
+                showLoading('#panel-form', 'Cargando', 'Enviando datos, por favor espere.');
                 var form = $("#forgot-pasword-form");
                 if ($(form).valid()) {
                     e.preventDefault();
                     var data = {
                         email: $('#EmailRemember').val()
                     }
-                    var actionError = (error) => {debugger
+                    var actionError = (error) => {
                         console.log(error);
                     }
                     var actionSuccess = (success) => {
-                        debugger
-                        console.log(success);
+                        hideLoading("#panel-form");
+                        var className = success.Code == 400 ? "" : "cancel-confirmation";
+                        var operationCancel = () => {
+                            success.Code == 200 && window.location.reload();
+                        }
+                        var buttons = AlertExec();
+                        showConfirmation(success.Message, buttons, className, operationCancel);
                     }
                     ajaxFunction(url, 'GET', data, actionError, actionSuccess);
                 }
             })
             $("#btnCancel").click(function (e) {
-                e.preventDefault()
-                box.hideAll();
+                e.preventDefault();
+                box.modal('hide');
             });
         });
     })
