98eb3a297da371e7dc181de3522612e40b0c8204
Luis David Alejandro Colmenares Rincon <ldcolmenares@indracompany.com>
2021-03-16 08:24:09 -0500

    test partial

---
+++ Gosocket.Dian.IndraTestProject/Controllers/OthersElectronicDocAssociatedControllerTests.cs
@@ -0,0 +1,129 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gosocket.Dian.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gosocket.Dian.Interfaces;
+using Moq;
+using Gosocket.Dian.Interfaces.Services;
+using Gosocket.Dian.Web.Models;
+using Gosocket.Dian.Domain.Common;
+using Gosocket.Dian.Domain.Entity;
+
+namespace Gosocket.Dian.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class OthersElectronicDocAssociatedControllerTests
+    {
+        private OthersElectronicDocAssociatedController _current;
+
+        private readonly Mock<IContributorService> contributorService = new Mock<IContributorService>();
+        private readonly Mock<IOthersDocsElecContributorService> othersDocsElecContributorService = new Mock<IOthersDocsElecContributorService>();
+        private readonly Mock<IOthersElectronicDocumentsService> othersElectronicDocumentsService = new Mock<IOthersElectronicDocumentsService>();
+        private readonly Mock<ITestSetOthersDocumentsResultService> testSetOthersDocumentsResultService = new Mock<ITestSetOthersDocumentsResultService>();
+        private readonly Mock<IOthersDocsElecSoftwareService> othersDocsElecSoftwareService = new Mock<IOthersDocsElecSoftwareService>();
+        private readonly Mock<IGlobalOtherDocElecOperationService> globalOtherDocElecOperationService = new Mock<IGlobalOtherDocElecOperationService>();
+
+        public void TestInitialize()
+        {
+            _current = new OthersElectronicDocAssociatedController(
+                  contributorService.Object,
+              othersDocsElecContributorService.Object,
+              othersElectronicDocumentsService.Object,
+              testSetOthersDocumentsResultService.Object,
+              othersDocsElecSoftwareService.Object,
+              globalOtherDocElecOperationService.Object
+                );
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+           var result= _current.Index(1);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void CancelRegisterTest()
+        {
+            var result = _current.CancelRegister(1, "Prueba");
+
+            Assert.IsNotNull(result); 
+        }
+
+        [TestMethod()]
+        public void EnviarContributorTest()
+        {
+            var data = new OthersElectronicDocAssociatedViewModel();
+            var result = _current.EnviarContributor(data);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetSetTestResultTest()
+        {
+            var result = _current.GetSetTestResult(1);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void SetTestDetailsTest()
+        {
+            var result = _current.SetTestDetails(1);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void CustomersListTest()
+        {
+            var result = _current.CustomersList(1, "", OtherDocElecState.Habilitado, 1, 10);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void SetupOperationModeTest()
+        {
+            var result = _current.SetupOperationMode(1);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void SetupOperationModePostTest()
+        {
+            var data = new OtherDocElecSetupOperationModeViewModel();
+            var result = _current.SetupOperationModePost(data);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void DeleteOperationModeTest()
+        {
+            var result = _current.DeleteOperationMode(1);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void RestartSetTestResultTest()
+        {
+            var data = new GlobalTestSetOthersDocumentsResult();
+            var result = _current.RestartSetTestResult(data, Guid.NewGuid());
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetSoftwaresByContributorIdTest()
+        {
+            var result = _current.GetSoftwaresByContributorId(1,1);
+            Assert.IsNotNull(result);
+        }
+
+        [TestMethod()]
+        public void GetDataBySoftwareIdTest()
+        {
+            var result = _current.GetDataBySoftwareId(Guid.NewGuid());
+            Assert.IsNotNull(result);
+        }
+    }
+}
---
+++ Gosocket.Dian.IndraTestProject/Controllers/OthersElectronicDocumentsControllerTests.cs
@@ -0,0 +1,103 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gosocket.Dian.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Moq;
+using Gosocket.Dian.Interfaces.Services;
+using Gosocket.Dian.Application;
+using Gosocket.Dian.Interfaces;
+
+namespace Gosocket.Dian.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class OthersElectronicDocumentsControllerTests
+    {
+
+        //private OthersElectronicDocumentsController _current;
+
+        //private readonly Mock<IOthersElectronicDocumentsService> othersElectronicDocumentsService = new Mock<IOthersElectronicDocumentsService>();
+        //private readonly Mock<IOthersDocsElecContributorService> othersDocsElecContributorService = new Mock<IOthersDocsElecContributorService>();
+        //private readonly Mock<IContributorService> contributorService = new Mock<IContributorService>();
+        //private readonly Mock<IElectronicDocumentService> electronicDocumentService = new Mock<IElectronicDocumentService>();
+        //private readonly Mock<IOthersDocsElecSoftwareService> othersDocsElecSoftwareService = new Mock<IOthersDocsElecSoftwareService>();
+
+        //[TestInitialize]
+        //public void TestInitialize()
+        //{
+        //    _current = new OthersElectronicDocumentsController(
+        //         othersElectronicDocumentsService.Object,
+        //     othersDocsElecContributorService.Object,
+        //     contributorService.Object,
+        //     electronicDocumentService.Object,
+        //     othersDocsElecSoftwareService.Object
+        //        );
+        //}
+
+
+        //[TestMethod()]
+        //public void IndexTest()
+        //{
+        //    _current.Index();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void AddOrUpdateTest()
+        //{
+        //    _current.AddOrUpdate();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void AddOrUpdateContributorTest()
+        //{
+        //    _current.AddOrUpdateContributor();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void AddParticipantsTest()
+        //{
+        //    _current.AddParticipants();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void AddTest()
+        //{
+        //    _current.Add();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void ValidationTest()
+        //{
+        //    _current.Validation();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetSoftwaresByContributorIdTest()
+        //{
+        //    _current.GetSoftwaresByContributorId();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void GetDataBySoftwareIdTest()
+        //{
+        //    _current.GetDataBySoftwareId();
+        //    Assert.Fail();
+        //}
+
+        //[TestMethod()]
+        //public void CancelRegisterTest()
+        //{
+        //    _current.CancelRegister();
+        //    Assert.Fail();
+        //}
+    }
+}
