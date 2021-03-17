4d3d8844445a3cf87625c2c1aad4364631b98e81
=rmaestre <=rjmaestre@indracompany.com>
2021-03-17 14:34:45 -0500

    RM: Test Unit

---
+++ Gosocket.Dian.IndraTestProject/Services/OthersDocsElecSoftwareServiceTests.cs
@@ -0,0 +1,74 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gosocket.Dian.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gosocket.Dian.Application.Tests
+{
+    [TestClass()]
+    public class OthersDocsElecSoftwareServiceTests
+    {
+        [TestMethod()]
+        public void OthersDocsElecSoftwareServiceTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetBySoftwareIdTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSoftwaresTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void ListTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void CreateSoftwareTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void DeleteSoftwareTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void SetToProductionTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSoftwareStatusNameTest()
+        {
+            Assert.Fail();
+        }
+
+        [TestMethod()]
+        public void GetSoftwaresByProviderTechnologicalServicesTest()
+        {
+            Assert.Fail();
+        }
+    }
+}
