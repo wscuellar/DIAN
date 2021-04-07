ae273a8f0fc92242f47bcea62cd7f3f8bb2c2235
Ricardo De Jesus Maestre Murgas <rjmaestre@indracompany.com>
2021-04-07 14:18:17 -0500

    RM: Ajuste de pruebas unitarias

---
+++ Gosocket.Dian.IndraTestProject/Controllers/ContributorCategoryControllerTests.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gosocket.Dian.Web.Controllers;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.Mvc;
+
+namespace Gosocket.Dian.Web.Controllers.Tests
+{
+    [TestClass()]
+    public class ContributorCategoryControllerTests
+    {
+        private ContributorCategoryController _currentController;
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _currentController = new ContributorCategoryController();
+        }
+
+        [TestMethod()]
+        public void IndexTest()
+        {
+            //arrange
+            //act
+            var viewResult = _currentController.Index() as ActionResult;
+            //assert
+            Assert.IsNotNull(viewResult);
+        }
+    }
+}
---
+++ Gosocket.Dian.IndraTestProject/Services/IdentificationTypeServiceTests.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using Gosocket.Dian.Application;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Gosocket.Dian.Web.Controllers;
+using Gosocket.Dian.Domain;
+
+namespace Gosocket.Dian.Application.Tests
+{
+    [TestClass()]
+    public class IdentificationTypeServiceTests
+    {
+        private IdentificationTypeService _currentController; 
+
+        [TestInitialize]
+        public void TestInitialize()
+        {
+            _currentController = new IdentificationTypeService();
+        }
+
+        [TestMethod()]
+        public void ListTest()
+        {
+            //arrange
+            //act
+            var viewResult = _currentController.List();
+            //assert
+            Assert.IsInstanceOfType(viewResult, typeof(IEnumerable<IdentificationType>)); 
+        }
+    }
+}
