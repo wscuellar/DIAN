c22bec750907da6cfd4a62b3cea89b5f9a44ad9f
Pedro Rozo <pedro.rozo@gmail.com>
2021-11-05 11:05:27 -0500

    FIX uso del contexto

--- L3 Application/Gosocket.Dian.Application/ContributorService.cs
+++ L3 Application/Gosocket.Dian.Application/ContributorService.cs
@@ -146,7 +146,10 @@ namespace Gosocket.Dian.Application
 
         public Contributor GetByCode(string code)
         {
-            return sqlDBContext.Contributors.FirstOrDefault(p => p.Code == code);
+            using (var context = new SqlDBContext())
+            {
+                return context.Contributors.FirstOrDefault(p => p.Code == code);
+            }
         }
 
         public RadianContributorOperation GetRadianOperations(int radianContributorId, string softwareId)
