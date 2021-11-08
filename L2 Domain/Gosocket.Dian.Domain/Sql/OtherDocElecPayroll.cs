e6983eb8f48d1253902ecb071381842ed6eb45d2
Juan David Gomez Forero <jdgomezf@indracompany.com>
2021-11-08 15:08:55 -0500

    Fix bug 10145: Adiciona clase para la entidad OtherDocElecPayroll de SQL

---
+++ L2 Domain/Gosocket.Dian.Domain/Sql/OtherDocElecPayroll.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Gosocket.Dian.Domain.Sql
+{
+    [System.ComponentModel.DataAnnotations.Schema.Table("OtherDocElecPayroll")]
+    public class OtherDocElecPayroll
+    {
+        public OtherDocElecPayroll()
+        {
+
+        }
+
+        [Key]
+        public Guid Id { get; set; }
+    }
+}
