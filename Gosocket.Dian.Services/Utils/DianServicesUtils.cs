8186a5a8b36cd77f7557252c2173e01a3c627d82
Samir Miranda <Bya_smiranda@outlook.com>
2022-04-26 16:16:17 -0500

    se corrige codigo de error NSAK21 para los documentos soportes y sus respectivas notas de ajuste para dar solucion al bug #14789

--- Gosocket.Dian.Services/Utils/DianServicesUtils.cs
+++ Gosocket.Dian.Services/Utils/DianServicesUtils.cs
@@ -678,6 +678,14 @@ namespace Gosocket.Dian.Services.Utils
             if (string.IsNullOrEmpty(responseXpathValues.XpathsValues["SenderCodeXpath"]))
             {
                 stringBuilder.AppendLine($"{codeMessage}J21: El NIT del Emisor no puede estar vacío.");
+                
+                /*Cuando el documento xml es de tipo documento soporte o su nota de ajuste el codigo de error de la regla que valida
+                 el senderCode es NSAK21 y no NSAJ21*/
+                if (new string[] { "05", "95" }.Contains(docTypeCode))
+                {
+                    stringBuilder.AppendLine($"{codeMessage}K21: El NIT del Emisor no puede estar vacío.");
+                }
+                
                 errors.Add(stringBuilder.ToString());
                 stringBuilder.Clear();
                 isValid = false;
@@ -1076,6 +1084,14 @@ namespace Gosocket.Dian.Services.Utils
                 if (string.IsNullOrEmpty(senderCode))
                 {
                     stringBuilder.AppendLine($"{codeMessage}J21: El NIT del Emisor no puede estar vacío.");
+
+                    /*Cuando el documento xml es de tipo documento soporte o su nota de ajuste el codigo de error de la regla que valida
+                        el senderCode es NSAK21 y no NSAJ21*/
+                    if (new string[] {"05", "95" }.Contains(docTypeCode))
+                    {
+                        stringBuilder.AppendLine($"{codeMessage}K21: El NIT del Emisor no puede estar vacío.");
+                    }
+                    
                     errors.Add(stringBuilder.ToString());
                     stringBuilder.Clear();
                     isValid = false;
